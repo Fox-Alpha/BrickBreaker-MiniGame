0c0a8b6ce42ef50ee35c2b5d9ae4517099cc8d4e
Fox-Alpha <fox-alpha@tarnhoerner.de>
2026-02-02 22:15:11 +0100

    Add: Dummy Levelszene

---
+++ scenes/game/main_game.gd
@@ -0,0 +1,26 @@
+extends Node2D
+
+@export var borders : WorldBoundarys
+@export var paddle : PackedScene
+@export var brickmap : TileMapLayer
+@export var cam : Camera2D
+
+var worldsize : Vector2i = Vector2i(800,800)
+@onready var vps := get_viewport_rect()
+
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+	borders.ResetWorldBorderPositions(get_viewport_rect().end)
+	cam.position = get_viewport_rect().get_center()
+	brickmap.position = get_viewport_rect().get_center()
+
+	var pad := paddle.instantiate()
+	add_child(pad)
+	var p:= Vector2i(vps.get_center().x, vps.end.y -pad.getPaddleSize().y -8)
+	pad.position = p
+	pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta: float) -> void:
+	pass
