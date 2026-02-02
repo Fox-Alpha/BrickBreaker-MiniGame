8009b53ad229f4794212eb9e542308542b2a91fd
Fox-Alpha <fox-alpha@tarnhoerner.de>
2026-02-02 22:14:44 +0100

    Add: Eub Paddle erstellt

---
+++ scenes/paddle/paddle_controller.gd
@@ -0,0 +1,49 @@
+extends CharacterBody2D
+
+
+@export_range(100.0,1000.0,10.0) var SPEED = 300.0
+
+@export_enum("Player_1", "Player_2") var PlayerPaddle = 0
+
+@export_color_no_alpha var playercolor
+@onready var paddle_collision: CollisionShape2D = $PaddleCollision
+
+
+func getPaddleSize() -> Vector2i:
+	return paddle_collision.shape.size
+
+func _ready():
+	#Game.connect("New_Game_Started", _reset_position, CONNECT_DEFERRED)
+	#Game.connect("Next_Round_Started", _reset_position, CONNECT_DEFERRED)
+	pass
+
+
+func _process(_delta):
+
+	pass
+
+
+func _physics_process(delta):
+	var direction
+	match PlayerPaddle:
+		0:
+			direction = Vector2(Input.get_action_strength("p1_right") - Input.get_action_strength("p1_left"), 0)
+		1:
+			direction = Vector2(0, Input.get_action_strength("p2_down") - Input.get_action_strength("p2_up"))
+
+	if(direction):
+		direction = direction.normalized()
+		velocity = direction * SPEED * delta
+		move_and_collide(velocity)
+
+func _reset_position():
+	var screensize = get_viewport_rect()
+
+	# Paddle Positionen
+	match PlayerPaddle:
+		0:
+			position = Vector2(10, screensize.size.y/2)
+		1:
+			position = Vector2(screensize.size.x-10, screensize.size.y/2)
+	#var col = Game.get_playercolor(PlayerPaddle)
+	#$Paddle.self_modulate = col
