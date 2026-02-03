0e8935ec72e6d600592de2715eb38650984e2c56
Fox-Alpha <fox-alpha@tarnhoerner.de>
2026-02-03 23:56:32 +0100

    Chg: Benennung und Group in tree entered verlegt

--- scenes/brick/brick.gd
+++ scenes/brick/brick.gd
@@ -1,11 +1,9 @@
-extends StaticBody2D
+class_name Brick extends StaticBody2D
 
 signal HitByBall
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
-	name = "Brick_" +str(get_instance_id())
-	add_to_group("Brick", true)
 	HitByBall.connect(_On_Hit_by_ball)
 	pass # Replace with function body.
 
@@ -16,12 +14,18 @@ func _ready() -> void:
 
 
 func _on_brick_area_body_shape_entered(_body_rid: RID, body: Node2D, _body_shape_index: int, _local_shape_index: int) -> void:
-	print("_on_brick_area_body_shape_entered: ", body.name)
+	#print("_on_brick_area_body_shape_entered: ", body.name)
 	HitByBall.emit()
 	pass # Replace with function body.
 
 
 func _On_Hit_by_ball() -> void:
-	print("Brick %s ist Hitted by Ball", [self.name])
+	#print("Brick %s ist Hitted by Ball", [self.name])
 	queue_free()
 	pass
+
+
+func _on_tree_entered() -> void:
+	name = "Brick_" +str(get_instance_id())
+	add_to_group("Brick", true)
+	pass # Replace with function body.
