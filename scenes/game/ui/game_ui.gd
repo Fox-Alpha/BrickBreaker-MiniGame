9b9e750f5ea7360908ba613f9c3f9a8aaeb0feda
Fox-Alpha <fox-alpha@tarnhoerner.de>
2026-02-07 00:20:23 +0100

    Add: Button für Reset

--- scenes/game/ui/game_ui.gd
+++ scenes/game/ui/game_ui.gd
@@ -1,6 +1,6 @@
 extends CanvasLayer
 
-
+@export var ball : RigidBody2D
 
 
 # Called when the node enters the scene tree for the first time.
@@ -11,3 +11,10 @@ func _ready() -> void:
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
 	pass
+
+
+func _on_button_pressed() -> void:
+	if(get_tree().current_scene.has_signal("Reset_Game")):
+		get_tree().current_scene.Reset_Game.emit()
+		pass
+	pass # Replace with function body.
