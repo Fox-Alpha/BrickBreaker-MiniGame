6afab13698d524739c8b3ab5bcf40f9aba508b88
Fox-Alpha <fox-alpha@tarnhoerner.de>
2026-02-07 00:23:42 +0100

    Add: Variante mit Claude AI unterstüzuung ===: Bessere Ball Collision Add: Maus Steuerung oder Tastatur

--- scenes/paddle/paddle_controller.gd
+++ scenes/paddle/paddle_controller.gd
@@ -1,20 +1,22 @@
-extends CharacterBody2D
+class_name PaddleController extends CharacterBody2D
 
 
 @export_range(100.0,1000.0,10.0) var SPEED = 650.0
 
-@export_enum("Player_1", "Player_2") var PlayerPaddle = 0
-
 @export_color_no_alpha var playercolor
 @onready var paddle_collision: CollisionShape2D = $PaddleCollision
 
+@export var paddle_width: float = 100.0
+
+# Für Maussteuerung
+@export var use_mouse := true
+@export var mouse_sensitivity: float = 1.0
 
 func getPaddleSize() -> Vector2i:
 	return paddle_collision.shape.size
 
 func _ready():
-	#Game.connect("New_Game_Started", _reset_position, CONNECT_DEFERRED)
-	#Game.connect("Next_Round_Started", _reset_position, CONNECT_DEFERRED)
+	$Paddle.self_modulate = playercolor
 	pass
 
 
@@ -24,26 +26,58 @@ func _process(_delta):
 
 
 func _physics_process(delta):
-	var direction
-	match PlayerPaddle:
-		0:
-			direction = Vector2(Input.get_action_strength("p1_right") - Input.get_action_strength("p1_left"), 0)
-		1:
-			direction = Vector2(0, Input.get_action_strength("p2_down") - Input.get_action_strength("p2_up"))
+	if use_mouse:
+		_mouse_move()
+	else:
+		_move_with_keyboard(delta)
 
-	if(direction):
-		direction = direction.normalized()
-		velocity = direction * SPEED * delta
-		move_and_collide(velocity)
+	# Bewegung ausführen
+	move_and_slide()
+
+func _mouse_move():
+	var mouse_pos = get_viewport().get_mouse_position()
+	var target_x = mouse_pos.x
+
+	# Sanfte Bewegung
+	position.x = lerp(position.x, target_x, mouse_sensitivity * 0.3)
+
+	# Velocity für Physics setzen (wichtig für Kollisionen)
+	velocity.x = (target_x - position.x) * 10
+	pass
+
+
+func _move_with_keyboard(delta):
+	var direction := 0.0
+
+	direction = Input.get_axis("p1_left", "p1_right")
+
+	if direction != 0:
+		velocity.x = direction * SPEED
+	else:
+		velocity.x = move_toward(velocity.x, 0, SPEED * delta * 5)
+
+	velocity = direction * SPEED * delta
+
+	#var collision := move_and_collide(velocity)
+	#if collision:
+		#pass
+
 
 func _reset_position():
-	var screensize = get_viewport_rect()
-
 	# Paddle Positionen
-	match PlayerPaddle:
-		0:
-			position = Vector2(10, screensize.size.y/2)
-		1:
-			position = Vector2(screensize.size.x-10, screensize.size.y/2)
+	position.x = get_viewport_rect().size.x/2
+
 	#var col = Game.get_playercolor(PlayerPaddle)
-	#$Paddle.self_modulate = col
+	$Paddle.self_modulate = playercolor
+
+
+func _on_area_2d_body_entered(body: Node2D) -> void:
+	if body.name == "Ball":  # Passe den Namen an deinen Ball an
+		# Berechne relativen Treffpunkt (-1 bis 1)
+		var hit_pos = (body.global_position.x - global_position.x) / getPaddleSize().x / 2
+
+		# Ändere Ball-Richtung basierend auf Treffpunkt
+		var bounce_angle = hit_pos * 60  # Max 60 Grad Ablenkung
+		var new_direction = Vector2(sin(deg_to_rad(bounce_angle)), -1).normalized()
+
+		body.linear_velocity = new_direction * body.linear_velocity.length()
