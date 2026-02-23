ba18f5d72ae3e0064e71ef4554e2ab259d3dd9e3
Fox-Alpha <Fox-Alpha@Tarnhoerner.de>
2026-02-23 22:04:38 +0100

    Rem: _process wird nicht benötigt

--- scenes/game/ui/game_ui.gd
+++ scenes/game/ui/game_ui.gd
@@ -14,11 +14,6 @@ func _ready() -> void:
 	pass # Replace with function body.
 
 
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta: float) -> void:
-	pass
-
-
 func _on_button_pressed() -> void:
 	if(get_tree().current_scene.has_signal("Reset_Game")):
 		get_tree().current_scene.Reset_Game.emit()
