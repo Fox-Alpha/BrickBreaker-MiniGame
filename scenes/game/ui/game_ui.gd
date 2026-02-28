96414725ed63eee9509aec1d00c24f6674efc0f7
Fox-Alpha <Fox-Alpha@Tarnhoerner.de>
2026-02-28 22:23:24 +0100

    Add: Main Menu aus anderem Projekt adoptiert

--- scenes/game/ui/game_ui.gd
+++ scenes/game/ui/game_ui.gd
@@ -2,14 +2,15 @@ extends CanvasLayer
 
 @export var ball : RigidBody2D
 
-@onready var label_score: Label = $PanelContainer/Panel/HBoxContainer/LabelScore
-@onready var reset_button: Button = $PanelContainer/Panel/HBoxContainer/ButtonResetGame
+@onready var label_score: Label = $VBoxContainer/PanelContainer/Panel/HBoxContainer/LabelScore
+@onready var reset_button: Button = $VBoxContainer/PanelContainer/Panel/HBoxContainer/ButtonResetGame
 @onready var color_rect: ColorRect = $VBoxContainer/ColorRect
 
 var _gamescore : int = 0
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
+	#color_rect.gui_input.connect(_on_color_rect_gui_input)
 	if get_tree().current_scene.has_signal("Player_Score"):
 		get_tree().current_scene.Player_Score.connect(_on_player_score)
 	if OS.has_feature("editor"):
@@ -21,11 +22,11 @@ func _ready() -> void:
 	else:
 		reset_button.hide()
 	if get_tree().current_scene.has_signal("GS_GAME_PAUSED"):
-		get_tree().current_scene.Player_Score.connect(_on_player_score)
+		get_tree().current_scene.GS_GAME_PAUSED.connect(_on_player_score)
 		# RECT Einblenden
 		pass
 	if get_tree().current_scene.has_signal("GS_GAME_UNPAUSED"):
-		get_tree().current_scene.Player_Score.connect(_on_game_unpaused)
+		get_tree().current_scene.GS_GAME_UNPAUSED.connect(_on_game_unpaused)
 		# RECT ausblenden
 		#
 		pass
@@ -53,4 +54,12 @@ func _on_game_unpaused() -> void:
 
 func _on_label_pause_gui_input(event: InputEvent) -> void:
 	# Unpause / Start Game
+	pass # Replace with function body.
+
+
+func _on_color_rect_gui_input(event: InputEvent) -> void:
+	if event.is_action_type():
+		if get_tree().paused:
+			get_tree().paused = false
+			get_tree().current_scene.GS_GAME_UNPAUSED.emit()
 	pass # Replace with function body.
